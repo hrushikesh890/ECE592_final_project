a25e76df3ccb5a380af0140e1c14b6df37efbd5a
Hrushikesh Patil <hrushikeshpatil890@gmail.com>
2021-11-19 11:43:15 -0500

    vmalloc

--- xinu/system/vmalloc.c
+++ xinu/system/vmalloc.c
@@ -1,48 +1,10 @@
 #include <xinu.h>
 
-// char* vmalloc(uint32 nbytes)
-// {
-// 	intmask mask;
-// 	struct vmemblk *prev, *curr, *leftover;
-	
-// 	mask = disable();
-
-// 	if(nbytes == 0){
-// 		restore(mask);
-// 		return (char *)SYSERR;
-// 	}
-
-// 	nbytes = (uint32) roundmb(nbytes);
-// 	prev = &ffslist;
-// 	curr = memlist.mnext;
-
-// 	while(curr != NULL){
-
-// 		if (curr->mlength == nbytes) {
-// 			prev->mnext = curr->mnext;
-// 			memlist.mlength -= nbytes;
-// 			restore(mask);
-// 			return (char *)(curr);
-
-// 		}
-
-// 		else if (curr->mlength > nbytes) {
-// 			leftover = (struct memblk *)((uint32) curr +
-// 					nbytes);
-// 			prev->mnext = leftover;
-// 			leftover->mnext = curr->mnext;
-// 			leftover->mlength = curr->mlength - nbytes;
-// 			memlist.mlength -= nbytes;
-// 			restore(mask);
-// 			return (char *)(curr);
-// 		}
-// 	}
-
-// }
 
 char* vmalloc(uint32 nbytes){
 	intmask mask;
 	pd_t* pd_base = proctab[currpid].pdbr;
+	pt_t* pt_base;
 
 	mask = disable();
 
@@ -54,22 +16,45 @@ char* vmalloc(uint32 nbytes){
 	}
 
 	//nbytes = (uint32) roundmb(nbytes);
-	uint32 npages = ceil(nbytes/4096);
+	uint32 npages = (nbytes/4096);
+	
+	if (nbytes > (npages*4096))
+	{
+		npages += 1;
+	}
+	
 	proctab[currpid].pages_alloc += npages;
 	int i;
-	for(i=9;i<1024;i++){
-		if(pd_base[i].pd_pres == 0){
+	for (i=9;i<1024;i++)
+	{
+		
+		if (pd_base[i].pd_pres == 0)
+		{
 			pd_base[i].pd_pres = 1;
 			pd_base[i].pd_write = 1;
-			pt_t* pt_base = (pt_t*)(getptmem(4096));
+			pt_base = (pt_t*)(getptmem(4096));
 			pd_base[i].pd_base = (uint32) (((uint32) pt_base >> 12) & (0x0FFFFF));
+		}
+		else
+		{
+			pt_base = (pt_t*)pd_base[i].pd_base;
+		}
 
-			for(i=0;i<1024;i++){
+		for(i=0; i<1024 ;i++)
+		{
+			if ((pt_base[i].pt_valid == 0) && (pt_base[i].pt_pres == 0) && (npages > 0))
+			{
 				pt_base[i].pt_pres = 0;
 				pt_base[i].pt_write = 1;
 				pt_base[i].pt_valid = 1;
+				npages -= 1;
 			}
 		}
+		if (npages == 0)
+		{
+			break;
+		}
+		
 	}
 
 	write_cr3((unsigned long) proctab[currpid].pdbr);
